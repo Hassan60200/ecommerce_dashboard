ad80f58721932f438191d0388ba63de2f559b58c
Hassan <derkaouihassan19@gmail.com>
2023-11-28 11:30:18 +0100

    create many component admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DasboardComponent } from './admin/dasboard/dasboard.component';
+import { ProductAdminComponent } from './admin/product-admin/product-admin.component';
+import { CategoryAdminComponent } from './admin/category-admin/category-admin.component';
+import { UserAdminComponent } from './admin/user-admin/user-admin.component';
+import { OrderAdminComponent } from './admin/order-admin/order-admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DasboardComponent,
+    ProductAdminComponent,
+    CategoryAdminComponent,
+    UserAdminComponent,
+    OrderAdminComponent
   ],
   imports: [
     BrowserModule,
