146718bb0cb7693ab49975727ab0d9304a8c5e56
Hassan <derkaouihassan19@gmail.com>
2023-12-04 18:14:36 +0100

    add function delete for category

--- src/app/admin/category-admin/category-admin.component.ts
+++ src/app/admin/category-admin/category-admin.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ApiManagerService} from "../../services/API/api-manager.service";
 import {Observable} from "rxjs";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-category-admin',
@@ -10,10 +11,25 @@ import {Observable} from "rxjs";
 export class CategoryAdminComponent implements OnInit {
   categories: Observable<any[]> | undefined;
 
-  constructor(private api: ApiManagerService) {
+  constructor(private api: ApiManagerService, private router: Router) {
   }
 
   ngOnInit() {
     this.categories = this.api.categoriesIndex();
+  }
+
+  onDelete(id: number) {
+    const isConfirmed = window.confirm('Are you sure you want to delete this employee?');
+    if (isConfirmed) {
+      this.api.deleteCategory(id).subscribe(
+        data => {
+          console.log(data);
+          this.router.navigateByUrl('/categories');
+        },
+        error => {
+          console.error('Error deleting category:', error);
+        }
+      );
+    }
   }
 }
